4761af32b1b35b1792ff4115859e6a1bde15357b
herwin <150341471+Ceche-Tulip@users.noreply.github.com>
2024-12-10 22:26:14 +0800

    引入JWT前后端交互（但存在跨域问题）

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,48 +1,33 @@
 <template>
-  <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </div>
 
-  <div class="mb-4">
-    <el-button plain>Plain</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="info" plain>Info</el-button>
-    <el-button type="warning" plain>Warning</el-button>
-    <el-button type="danger" plain>Danger</el-button>
-  </div>
-
-  <div class="mb-4">
-    <el-button round>Round</el-button>
-    <el-button type="primary" round>Primary</el-button>
-    <el-button type="success" round>Success</el-button>
-    <el-button type="info" round>Info</el-button>
-    <el-button type="warning" round>Warning</el-button>
-    <el-button type="danger" round>Danger</el-button>
-  </div>
-
-  <div>
-    <el-button :icon="Search" circle />
-    <el-button type="primary" :icon="Edit" circle />
-    <el-button type="success" :icon="Check" circle />
-    <el-button type="info" :icon="Message" circle />
-    <el-button type="warning" :icon="Star" circle />
-    <el-button type="danger" :icon="Delete" circle />
-  </div>
+    <el-button type="primary" @click="handleLogin">测试登陆</el-button>
+    <el-button type="success" @click="handleUserList">测试获取用户信息列表</el-button>
 </template>
 
 <script setup>
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Star,
-} from '@element-plus/icons-vue'
+import requestUtil from '@/util/request'
+
+const handleLogin=async()=>{
+  let result = await requestUtil.get("user/jwt_test")
+  let data = result.data;
+  if (data.code == 200) {
+    const token = data.token
+    console.log("登陆成功,token=" + token)
+    window.sessionStorage.setItem("token", token)
+  } else {
+    console.log("登陆出错")
+  }
+}
+
+const handleUserList=async()=>{
+  let result = await requestUtil.get("user/test")
+  let data = result.data;
+  if (data.code == 200) {
+    const userList = data.data;
+    console.log("用户信息列表，userList=" + userList)
+  } else {
+    console.log("登陆出错")
+  }
+}
+
 </script>
