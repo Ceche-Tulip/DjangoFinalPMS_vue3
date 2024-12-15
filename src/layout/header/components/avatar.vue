48d97a7b90bdc029ab37abf7acaaac709aff1a58
herwin <150341471+Ceche-Tulip@users.noreply.github.com>
2024-12-15 23:48:22 +0800

    右上角用户头像显示的实现

---
+++ src/layout/header/components/avatar.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-dropdown>
+    <span class="el-dropdown-link">
+      <el-avatar shape="square" :size="40" :src="squareUrl" />
+      &nbsp;&nbsp;{{currentUser.username}}
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>个人中心</el-dropdown-item>
+        <el-dropdown-item @click="logout">安全退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import { ArrowDown } from '@element-plus/icons-vue'
+import requestUtil, {getServerUrl} from '@/util/request'
+import router from '@/router'
+const currentUser = JSON.parse(sessionStorage.getItem("currentUser"))  // currentUser 为用户信息
+
+const squareUrl = getServerUrl() + 'media/userAvatar/' + currentUser.avatar
+// const squareUrl = getServerUrl() + 'media/userAvatar/' + '1.jpg'
+
+const logout=()=>{
+  window.sessionStorage.clear()
+  router.replace('/login')
+}
+</script>
+
+
+<style lang="scss" scoped>
+                   .el-dropdown-link {
+                     cursor: pointer;
+                     color: var(--el-color-primary);
+                     display: flex;
+                     align-items: center;
+                   }
+</style>
---
+++ src/layout/header/components/breadcrumb.vue
@@ -0,0 +1,12 @@
+<template>
+  面包屑
+</template>
+
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
